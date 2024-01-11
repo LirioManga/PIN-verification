22671fd8b345e8945b7d73ca6cb1e7aff43338c7
Lírio Manga <147447200+LirioManga@users.noreply.github.com>
2024-01-11 11:05:35 +0200

    :bug: fix: Qualquer resultado depois do correto, aceite como correto

--- script.js
+++ script.js
@@ -1,13 +1,71 @@
 let pin;
-var input = document.querySelector("input");
-var buttonVerification = document.getElementById("buttonVerification");
+let inputPin;
 
+let input = document.querySelector("input");
+var buttonVerification = document.getElementById("buttonVerification");
+let buttonTentar = document.getElementById("buttonTentar");
+
+const pinGerado = Math.floor(Math.random()*1000+1000);
+const classificacao = document.querySelector("#classificacao");
 
 buttonVerification.onclick = verificarPin;
 
 function verificarPin(){
-    pin = document.getElementById("ipin").value;
 
-   
+    inputPin = document.getElementById("ipin").value;
+    pin = parseInt(inputPin);
+
+    console.log(pinGerado);
+
+    if (inputPin == ""){
+        input.style.borderColor ='red';
+        input.focus();
+
+    }
+
+    if(pin > 999){
+
+        if(pin < pinGerado){
+
+            classificacao.textContent = 'PIN incorrecto, tente um numero maior';
+            classificacao.style.display = 'block';
+            input.focus();
+            tenteNovamente();
+
+        }else if(pin > pinGerado){
+
+            classificacao.textContent = "PIN incorrecto, tente um numero menor"
+            classificacao.style.display = 'block';
+            input.focus();
+            tenteNovamente();
+        
+
+        } else if(pin === pinGerado){
+            classificacao.style.color = 'blue';
+            classificacao.textContent ='Pin correcto';
+            classificacao.style.display = 'block';
+            buttonTentar.style.display = 'none';
+            buttonVerification.style.display = 'none';
+            input.style.color = 'blue';
+            input.style.borderColor = 'blue'
+            
+        }
+    } else{
+        alert("Introduza um numero com 4 Dígitos");
+    }
+
+}
+
+
+function tenteNovamente(){
+
+    buttonVerification.style.display = 'none';
+    buttonTentar.style.display='block';
+    input.style.borderColor = 'red';
+    input.style.color= 'red';
+    
+
+    buttonTentar.onclick = verificarPin;
+    
 }
 
